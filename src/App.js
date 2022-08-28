48f5a6ce14e50ba4ab8332561695fcaac3894e34
shivamsri27 <101566482+shivamsri27@users.noreply.github.com>
2022-08-28 19:04:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,232 @@
+import './App.css';
+import {FaSearch, FaCartArrowDown, FaUserTie } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="App">
+     
+      <div className='ban2'>
+        <div> <img className='logo' src="https://images.ctfassets.net/5de70he6op10/53ZOE4rRqrxcvv0hg2eSLV/a849085e5f600c618132be2475017746/anthro-logo.svg" alt="BigCo Inc. logo"/></div>
+        <div> <img className='logo2' src="https://images.ctfassets.net/5de70he6op10/y1O9tEDOvCGliGUvT9RoJ/eb8ab4734c5e3778b8b61da70ed441ac/AL_NewLogo_2.svg" alt="BigCo Inc. logo"/></div>
+        <div className='searchbox'>
+          <input type="text" placeholder="Search Anthropologie"  />
+          <button className='sb'><FaSearch /></button>
+          <div className='cardlogo'>< FaCartArrowDown />  </div>
+          <div className='cardlogo'> <FaUserTie /></div>
+        </div>
+        <div></div>
+      </div>
+      <div className='navebar'>
+        <div><p>New!</p></div>
+        <div><p>Dresses</p></div>
+        <div><p>Clothing</p></div>
+        <div><p>Shoes</p></div>
+        <div><p>Accessories</p></div>
+        <div><p>Gifts & Candles</p></div>
+        <div><p>Home & Furniture</p></div>
+        <div><p>Beauty & Wellness</p></div>
+        <div><p>Garden & Outdoor</p></div>
+        <div><p>Wedding</p></div>
+        <div><p>Sale</p></div>
+      </div>
+      <div><img className='ban1' src="https://images.ctfassets.net/5de70he6op10/4Av1j3pbCiZExz3bNjPHmP/c9fe1f21f460ff459857a395560b9199/082522_30OffBeddingBathRugsCurtains_CreativePrimaryBanner_LS_01.jpg" alt="BigCo Inc. logo"/></div>
+      <div className='fall'>
+        
+      <img src="https://images.ctfassets.net/5de70he6op10/3VC51Wpuyoa3KgQDT4aZsS/8fa5147c3faf98a20e2185bef3dff107/LS_M0.jpg?w=2694&q=80&fm=webp" alt="BigCo Inc. logo" />
+      </div>
+      <div className='imgban1'>
+        <div className='imge'>
+        <img src="https://images.ctfassets.net/5de70he6op10/167rhLDYGG4umOBC9KiQYh/1b971d445595af2087676177e1b2703d/LS_M1a.jpg?w=856&q=80&fm=webp" alt="BigCo Inc. logo" />
+        <button className='imgbtn'><p>shop new Clothing + accessories</p></button>
+        </div>
+        <div className='imge'>
+        <img src="https://images.ctfassets.net/5de70he6op10/2yywsh5Tmi0RZIHxaFM4LD/5b63c5642f0fd4c3166aef725fc72e67/LS_M1b.jpg?w=856&q=80&fm=webp" alt="BigCo Inc. logo" />
+        <button className='imgbtn2'><p>shop tops</p></button>
+        </div>
+        <div className='imge'>
+        <img src="https://images.ctfassets.net/5de70he6op10/261xhq4wDbiuPONBeDFU36/76fcf00868e9f3d13911ab32892da6f8/LS_M1c.jpg?w=856&q=80&fm=webp" alt="BigCo Inc. logo" />
+        <button className='imgbtn3'><p>shop dresses</p></button>
+        </div>
+        <div className='imge'>
+        <img src="https://images.ctfassets.net/5de70he6op10/2MCrpgTOHt98PRQoZ0FgAN/f1dcb908c505bf267f52278e71d0d27b/LS_M2a.jpg?w=856&q=80&fm=webp" alt="BigCo Inc. logo" />
+        <button className='imgbtn4'><p>shop boots</p></button>
+        </div>
+        <div className='imge'>
+        <img src="https://images.ctfassets.net/5de70he6op10/4yWotyhGrGoTlrjRxv2Q7Y/e659ba3be2897f62f0f747abde5ebc07/LS_M2b.jpg?w=856&q=80&fm=webp" alt="BigCo Inc. logo" />
+        <button className='imgbtn5'><p>shop new  accessories</p></button>
+        </div>
+        <div className='imge'>
+        <img src="https://images.ctfassets.net/5de70he6op10/1QQAxAOJVOhQlpxkW8fHnI/152657c334f68098bff17a44078ed360/LS_M2c.jpg?w=856&q=80&fm=webp" alt="BigCo Inc. logo" />
+        <button className='imgbtn6'><p>shop pants</p></button>
+        </div>
+      </div>
+      <div className='livingroom'>
+        <div>
+          <img src="https://images.ctfassets.net/5de70he6op10/5JhjcmPBuPSEb8FMLX6saJ/ae5e83bf2d7c01b6bc317b6218f28e07/LS_M3a.jpg?w=1756&q=80&fm=webp" alt="logo" />
+          <button className='imgbtn7'><p>explore the exclusive collab</p></button>
+        </div>
+        <div>
+          <img src="https://images.ctfassets.net/5de70he6op10/6RIAy1GvAtHvLWJXjpQ4SK/a582e679fc7fdd412b7b436788a452d3/LS_M3b.jpg?w=856&q=80&fm=webp" alt="logo" />
+          <button className='imgbtn8'><p>take the fall 2022 house tour</p></button>
+        </div>
+      </div>
+      <div className='gold'>
+        <img src="https://images.ctfassets.net/5de70he6op10/51jxVcrlgwRBy7jGQsReWZ/6f93be96e56d503f20e1138ac0124dd6/LS_M4.jpg?w=2694&q=80&fm=webp" alt="logo"/>
+        <button className='imgbtn9'><p>shop dresses</p></button>
+      </div>
+      <div className="shop">
+        <div>
+          <img src="https://images.ctfassets.net/5de70he6op10/6Vs1tWuvDr4VH2IL8BtlDa/f90d4888cf7e8e1436eb4fffb653f784/RR_sweaters.jpg?w=856&q=80&fm=webp" alt="logo" />
+          <button className='imgbtn10'><p>shop dresses</p></button>
+        </div>
+        <div>
+          <img src="https://images.ctfassets.net/5de70he6op10/2MfDHKENHTX4LnaiNReUCZ/e2b4384e507722496e92ab224a6ff6de/RR_Kitchen_Dining.jpg?w=856&q=80&fm=webp" alt="logo" />
+          <button className='imgbtn11'><p>shop dresses</p></button>
+        </div>
+        <div>
+          <img src="https://images.ctfassets.net/5de70he6op10/6nklHli9n16QG5uk7F0XEE/cede8948e803d7958225a1f0cfd0b2e6/RR_Bedding.jpg?w=856&q=80&fm=webp" alt="logo"/>
+          <button className='imgbtn12'><p>shop dresses</p></button>
+        </div>
+      </div>
+      <div className='Trend'>
+        <p>Trending Now</p>
+      </div>
+      <div className='grid'>
+        <div>
+          <img src='https://images.urbndata.com/is/image/Anthropologie/4133293120007_009_b2?$an-category$&qlt=80&fit=constrain' alt="logo"/>
+        </div>
+        <div>
+        <img src='https://images.urbndata.com/is/image/Anthropologie/78395357_041_b2?$an-category$&qlt=80&fit=constrain' alt="pogo"/>
+        </div>
+        <div>
+        <img src='https://images.urbndata.com/is/image/Anthropologie/68837335_066_b2?$an-category$&qlt=80&fit=constrain' alt="logo"/>
+        </div>
+        <div>
+        <img src='https://images.urbndata.com/is/image/Anthropologie/68739671_000_b2?$an-category$&qlt=80&fit=constrain' alt="logo"/>
+        </div>
+        <div>
+        <img src='https://images.urbndata.com/is/image/Anthropologie/78303617_055_b?$an-category$&qlt=80&fit=constrain' alt="logo"/>
+        </div>
+      </div>
+      <div className='more'>
+        <p>More to Explore</p>
+      </div>
+      <div className='moregrid'>
+        <div>
+          <img src="https://images.ctfassets.net/5de70he6op10/3pW03PezVotGmhIJvEkirV/22edfed33d97b983fe8f83c9876cd43f/MTE_DenimFitGuide.jpg?w=856&q=80&fm=webp" alt='logo'/>
+          <h3 className='good'>Good Jeans: The Denim Fit Guide</h3>
+          <p>Meet the perfect pairs for every wear. From rise to size, here's how to find your new favorite jeans.</p>
+          <p className='good2'>find your fit</p>
+        </div>
+        <div>
+        <img src="https://images.ctfassets.net/5de70he6op10/4KdZlgMAtV7l3IUUu24UNs/4db32dcfcdb43ebcd7c505c45e8f8183/MTE_NatalieL.jpg?w=856&q=80&fm=webp" alt='logo'/>
+        <h3 className='good'>Nathalie Lété for Anthropologie</h3>
+          <p>Meet the perfect pairs for every wear. From rise to size, here's how to find your new favorite jeans.</p>
+          <p className='good2'>find your fit</p>
+        
+        </div>
+        <div>
+        <img src="https://images.ctfassets.net/5de70he6op10/2oIntG5FdfvDGzn3VJWT7T/0c6fd2ab04fcc5f76bfa6801968081f0/MTE_3.jpg?w=856&q=80&fm=webp" alt='logo'/>
+        <h3 className='good'>Fall, Aglow</h3>
+          <p>Meet the perfect pairs for every wear. From rise to size, here's how to find your new favorite jeans.</p>
+          <p className='good2'>find your fit</p>
+        </div>
+      </div>
+      <div><p>* Exclusions apply. See details.</p></div>
+      <div className='line'></div>
+      <div className='about'>
+        <h2>About Us</h2>
+        <p>Our mission at Anthropologie has always been to surprise and delight you with unexpected, distinctive finds for your closet and home. We source and craft all of our products with care, </p> 
+        <p className='ensur'>ensuring that any treasure you find at Anthropologie is unique, just like you. Explore our dress shop to find styles and fits perfect for any occasi...</p>
+      </div>
+      <div className='email'>
+        <div>
+          <div>
+            <h3 className='sign'>Sign Up for Email</h3>
+            <p className='rec'>Receive early access to new arrivals, sales, exclusive content, events</p> 
+            <p className='and'>and much more!</p>
+          </div>
+        </div>
+        <div>
+        <input  className='type' type="text" placeholder='enter your gmail' />
+        <button className='submit'>Submit</button>
+        </div>
+      </div>
+      <div className='by'>
+        <p>By signing up, you will receive Anthropologie offers, promotions and other commercial messages. You are also agreeing to Anthropologie’s Privacy Policy. You may unsubscribe at any time.</p>
+      </div>
+      <div className='footer'>
+        <div>
+          <div><h4>Help</h4></div>
+          <div><p>Track Your Order</p></div>
+          <div><p>Returns & Exchanges</p></div>
+          <div><p>Shipping</p></div>
+          <div><p>Customer Service</p></div>
+          <div><p>Current Promotions</p></div>
+          <div><p>Product Recalls</p></div>
+          
+        </div>
+        <div>
+        <div><h4>About Us</h4></div>
+          <div><p>Our Story</p></div>
+          <div><p>A Greater Good</p></div>
+          <div><p>Diversity & Inclusion</p></div>
+          <div><p>Stories</p></div>
+          <div><p>Events</p></div>
+        </div>
+        <div>
+        <div><h4>Services</h4></div>
+          <div><p>AnthroPerks</p></div>
+          <div><p>Gift Cards</p></div>
+          <div><p>AnthroLiving B2B</p></div>
+          <div><p>Furniture: Guides & Services</p></div>
+          <div><p>Store Pickup & Collection Points</p></div>
+          <div><p>Klarna</p></div>
+        </div>
+        <div>
+        <div><h4>Connect</h4></div>
+          <div><p>Contact Us</p></div>
+          <div><p>Stay Connected</p></div>
+          <div><p>Careers</p></div>
+          <div><p>Styling Services</p></div>
+          <div><p>Request A Catalog</p></div>
+        </div>
+        <div>
+          <div><p>Store Locator</p></div>
+          <div><p>Get Email</p></div>
+        </div>
+      </div>
+      <div className='socalmedia'>
+        <div>
+          <img src='https://images.ctfassets.net/5de70he6op10/4gWq1G4qLLgmqlNeHfzV2T/55298745127c36c5876f4013434c0c37/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917_Copy_2.svg' alt='logo'/>
+        </div>
+        <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png' alt='logo'/>
+        </div>
+        <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png' alt='logo'/>
+        </div>
+        <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/640px-Facebook_logo_%28square%29.png' alt='logo'/>
+        </div>
+        <div>
+        <img src='https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png' alt='logo'/>
+        </div>
+      </div>
+      <div className='lastbanner'>
+        <img src='https://images.ctfassets.net/5de70he6op10/2DRgCRXNIEkk0wIqUo2ywa/20ea44a95fb361eaf034b42eec179d44/_site_footer__download_app_enable_push_.jpg' alt='banner' />
+      </div>
+      <div className='country'>
+        <div><p>US</p></div>
+        <div><p>France</p></div>
+        <div><p>Germany</p></div>
+        <div><p>Italy</p></div>
+        <div><p>Spain</p></div>
+        <div><p>UK</p></div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
